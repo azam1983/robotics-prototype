ce9b26d5a5ba667e241f9a150ced8c16b567f2d2
PeterGhimself <petergranitski@gmail.com>
2017-10-11 20:11:27 -0500

    refactoring of max's code

--- motor-interface/arm-prototype/arm-claw-no-IR/armClawNoIR/armClawNoIR.ino
+++ motor-interface/arm-prototype/arm-claw-no-IR/armClawNoIR/armClawNoIR.ino
@@ -1,32 +1,44 @@
 #include <Servo.h>
  
-Servo myservo;  // create servo object to control a servo
-Servo myservo1;
+Servo pitchServo;  // create servo object to control a servo, pitch
+Servo rollServo; // roll
  
 int potpin = 1;  // analog pin used to connect the potentiometer
 int potpin1 = 4;
-int val,value;    // variable to read the value from the analog pin
- 
+int pitchPotentioVal,rollPotentioVal;    // variables to read the value from the analog pin
+const int MIN_VAL = 10;
+const int MAX_VAL = 122;
+
 void setup() {
-  Serial.begin(9600);
-  myservo.attach(2);  // attaches the servo on pin 2 to the servo object
-  myservo1.attach(4);
+    Serial.begin(9600);
+    pitchServo.attach(2);  // attaches the servo on pin 2 to the servo object
+    rollServo.attach(4);
 }
  
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  value = analogRead(potpin1);
-  value = map(value, 0, 1023, 0, 180);  
-  if(value < 10) value = 10;
-  if(value > 122) value = 121;
-  Serial.print("Value of wrist roll angle: ");
-  Serial.println(value);
-  myservo1.write(value);
-  //if(val > 105) val=140;
-  //if(val < 85) val=80;
-  Serial.print("Value of wrist pitch angle: ");
-  Serial.println(val);
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+    pitchPotentioVal = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+    pitchPotentioVal = map(pitchPotentioVal, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+    rollPotentioVal = analogRead(potpin1);
+    rollPotentioVal = map(rollPotentioVal, 0, 1023, 0, 180);  
+  
+    if (rollPotentioVal < MIN_VAL) {
+        rollPotentioVal = 10;
+    }
+    
+    if (rollPotentioVal > MAX_VAL) {
+        rollPotentioVal = 121;
+    }
+    
+    Serial.print("Value of wrist roll angle: ");
+    Serial.println(rollPotentioVal);
+    rollServo.write(rollPotentioVal);
+    
+    //if(val > 105) val=140;
+    //if(val < 85) val=80;
+    
+    Serial.print("Value of wrist pitch angle: ");
+    Serial.println(pitchPotentioVal);
+    pitchServo.write(pitchPotentioVal);  // sets the servo position according to the scaled value
+    delay(15);                           // waits for the servo to get there
 }
+
